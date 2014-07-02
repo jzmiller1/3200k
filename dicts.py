7cd3914413c42a095d1be6d1128070506c30427d
Zac Miller <zmiller1@gmail.com>
2014-07-02 13:36:24 -0400

    Add skeletons of hist, str_to_int and null_list functions.

---
+++ dicts.py
@@ -0,0 +1,42 @@
+__author__ = 'zmiller'
+
+
+def hist(s):
+    """returns the histogram of the characters in s
+
+    >>> hist("AAGGT")
+    {'A': 2, 'T': 1, 'G': 2}
+
+    >>> hist("!!xx")
+    {'!': 2, 'x': 2}
+
+    """
+    pass
+
+
+def str_to_int(s):
+    """converts a string to an integer value
+
+    >>> str_to_int('s')
+    115
+
+    >>> str_to_int('st!ll')
+    11511633108108L
+
+    hint: the built in ord and chr functions
+
+    """
+    pass
+
+
+def null_list(length):
+    """return a list of all None values of given length
+
+    >>> null_list(3)
+    [None, None, None]
+
+    >>> null_list(1)
+    [None]
+
+    """
+    pass
